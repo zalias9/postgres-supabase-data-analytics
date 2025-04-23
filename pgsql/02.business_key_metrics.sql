5b93d6378c02f32fb505a8702ca75fd984586c11
Zaid Ahmed <zaidahmed@gmail.com>
2025-04-24 02:09:57 +0400

    feat: add business key metrics script

---
+++ pgsql/02.business_key_metrics.sql
@@ -0,0 +1,69 @@
+WITH
+  metrics AS (
+    SELECT
+      SUM(sales_amount)::money AS total_sales,
+      SUM(quantity) AS total_quantity,
+      ROUND(AVG(price), 2)::money AS avg_price,
+      COUNT(distinct order_number) AS total_orders
+    FROM
+      gold.fact_sales
+  ),
+  product_metrics AS (
+    SELECT
+      COUNT(DISTINCT product_name) AS total_products
+    FROM
+      gold.dim_products
+  ),
+  customer_metrics AS (
+    SELECT
+      COUNT(customer_key) AS total_customers
+    FROM
+      gold.dim_customers
+  )
+SELECT
+  'Total Sales' AS measure_name,
+  total_sales::text AS measure_value
+FROM
+  metrics
+UNION ALL
+SELECT
+  'Total Quantity',
+  total_quantity::text
+FROM
+  metrics
+UNION ALL
+SELECT
+  'Average Price',
+  avg_price::text
+FROM
+  metrics
+UNION ALL
+SELECT
+  'Total Orders',
+  total_orders::text
+FROM
+  metrics
+UNION ALL
+SELECT
+  'Total Products',
+  total_products::text
+FROM
+  product_metrics
+UNION ALL
+SELECT
+  'Total Customers',
+  total_customers::text
+FROM
+  customer_metrics;
+
+/* Results:
+====================================
+| measure_name    | measure_value  |
+| --------------- | -------------- |
+| Total Sales     | $29,356,250.00 |
+| Total Quantity  | 60423          |
+| Average Price   | $486.04        |
+| Total Orders    | 27659          |
+| Total Products  | 295            |
+| Total Customers | 18484          |
+*/
